37a07b01d31b86df44efb7de0e4f946498587280
Jjorgealvaradocrispin <jalvaradocrispin@gmail.com>
2022-08-12 10:48:22 -0600

    Ejercicio 2

---
+++ paginicio.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="diseño1.css">
+    <link rel="stylesheet" href="diseño2.css">
+    <title>Adidas</title>
+</head>
+<table width="100%">
+ <tr>
+ <td colspan="12" align="center">
+ <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+ <div class="carousel-indicators">
+ <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+ <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+ <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+ <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4  "></button>
+ </div>
+ <div class="carousel-inner">
+ <div class="carousel-item active">
+ <img src="images/n3.jpeg" class="d-block w-90" alt="...">
+ </div>
+ <div class="carousel-item">
+      <img src="images/n2.jpeg" class="d-block w-90" alt="...">  
+      </div>
+      <div class="carousel-item">
+      <img src="images/n1.jpeg" class="d-block w-90" alt="...">
+      </div>
+      <div class="carousel-item">
+      <img src="images/n4.jpeg" class="d-block w-90" alt="...">
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+      </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  <span class="visually-hidden">Next</span><br>
+  </button><br>
+  </div><br><br>
+      <table style="border-collapse: collapse; font-family: 'Times New Roman', Times, serif;" border="1px" width="100%">
+      <tbody><tr>
+      <td style="'Times New Roman', Times, serif; text-align: center; background-color: #ffe100;"><a href="productos.html" style="text-decoration: none; color: rgb(28, 27, 23);"><b>PRODUCTOS</b></a></td>
+      <td style="'Times New Roman', Times, serif; text-align: center; background-color: #00aeff;"><a href="tiendas.html" style="text-decoration: none; color: rgb(28, 27, 23);"><b>TIENDAS</b></a></td>
+      <td style="'Times New Roman', Times, serif; text-align: center; background-color: #0c5bb0;"><a href="contacto/.html" style="text-decoration: none; color: rgb(30, 28, 27);"><b>CONTACTENOS</b></a></td>
+    </tr>
+  </tbody></table><br><br>
+        </tr>
+        <td colspan="12" align="left"><br>
+                <h3 style="color: black;"><b>NUESTRAS OFERTAS DE MES</b></h3>
+            </td>
+    </table><br>
+    <div class="contenedor">
+        <section class="a" align="center"><a href="images/logo.png"><img src="images/adidas.png" width="330px" height="300px"></a></section>
+        <section class="b" align="center"><img src="images/zaa1.jpg" width="300px" height="250px"></section>
+        <section class="c" align="center"><img src="images/zaa2.jpg" width="300px" height="250px"></section>
+        <section class="d" align="center"><img src="images/zaa3.jpg" width="300px" height="200px"></section>
+    </div>
+    <table align="center" width="100%">
+        <tr style="background-color: white;">
+            <td colspan="3" align="left">
+                <h6 style="color: withe;"><b>PBX: XXXXX-XXXXX</b> </h6>
+            </td>
+            <td colspan="3" align="right">
+                <h6 style="color:withe  left;"><b>Dirección: XXXXX-XXXXX-XXXX</b></h6>
+            </td>
+            <td colspan="3" align="right">
+                <h6><b>Carné: 2018-40162</b></h6>
+            </td>
+            <td colspan="4">.</td>
+            <td colspan="3" align="right">
+                <h6><b>Jorge Luis Alvarado Crispin </b></h6>
+            </td>
+        </tr>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
